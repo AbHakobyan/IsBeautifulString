cdf4a8cdf1717dae26227c36cfc277e8c48d2074
Albert Hakobyan <abhakobyan24@gmail.com>
2021-10-11 12:30:43 +0400

    Добавьте файлы проекта.

---
+++ TestFin/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "ckfbc";
+            bool res = isBeautifulString(s);
+            Console.WriteLine(res);
+        }
+
+        static bool isBeautifulString(string inputString)
+        {
+            int[] count = new int[26];
+            foreach (var s in inputString)
+            {
+                count[s - 97]++;
+            }
+            for (int i = 0; i < 25; ++i)
+            {
+                if (count[i] < count[i + 1]) return false;
+            }
+            return true;
+        }
+    }
+}
